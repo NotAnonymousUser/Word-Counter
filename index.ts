9c5a8e2f59a91f06cf0eff91fa3a5b75738d1462
Muhammad Mubeen Javaid <125754246+NotAnonymousUser@users.noreply.github.com>
2024-05-14 14:11:26 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt({
+  name: "sentence",
+  type: "input",
+  message: "Enter words you wanted to be count",
+});
+
+let counter = answer.sentence.trim().split(" ");
+let wordCounter = counter.length;
+
+console.log(`Your sentence has ${wordCounter} words`);
